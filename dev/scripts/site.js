37ae64fe819898f02663eba8f2faf72aecf37a13
Paul Bassi <paulbassi7@gmail.com>
2018-06-06 15:25:43 -0400

    Added styles and responsive media queries

--- dev/scripts/site.js
+++ dev/scripts/site.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
-    window.location.hash= '';
+$(document).ready(function () {
+    $(this).scrollTop(0);
+    $(document).scrollTop(0);
+    window.location.hash = '';
     console.log(`ready`);
 });
+
+
+
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
+$(function () {
+    $('a').smoothScroll({
+        speed: 700,
+    });
+});
+
